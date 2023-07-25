328c704a4338c9362aa981df4bcb59031ea9072b
Thomas Szentirmay <tlszent@outlook.com>
2023-07-25 12:05:27 -0400

    added connection check

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Pull in mongod package
 const { MongoClient } = require('mongodb');
 
+// Connection URL
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
+// Check connection
 client.connect().then(() => {
     console.log('Client connected');
 })
