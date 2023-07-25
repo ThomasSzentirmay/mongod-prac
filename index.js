06492fda8cd534c28f98a6f4f7ceba5a7442cd94
Thomas Szentirmay <tlszent@outlook.com>
2023-07-25 12:53:11 -0400

    connected to exisitng db and collection, and found all data in that referenced collection

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ const client = new MongoClient(url);
 // Check connection
 client.connect().then(() => {
     console.log('Client connected');
+
+    // Reference the DB and Collection you want to interact with
+    const db = client.db('rutgers_db');
+
+    const studentsCollection = db.collection('students');
+
+    studentsCollection
+    .find()
+    .toArray()
+    .then(students => {
+        console.log(students);
+    })
 })
