738e7d02e5341d84d3373b4d898c4239ef6922ef
Thomas Szentirmay <tlszent@outlook.com>
2023-07-26 11:29:59 -0400

    visualising our API models

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,26 @@
 const db = require('./connection');
+const {Planet, Like} = require('../models');
+const axios = require('axios');
+
+const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+const planetData = [];
+
 
 db.once('open', () => {
     console.log('db connected');
+
+    planets.forEach(async (planet, index) => {
+        const url = `https://planets-17f2.onrender.com/planets/getPlanet?name=${planet}`;
+        const {data} = await axios.get(url);
+
+        planetData.push({
+            name: data.name,
+            picture: data.picture,
+            description: data.description
+        });
+
+        if(index === planets.length - 1) {
+            console.log(planetData);
+        }
+    });
 });
--- server.js
+++ server.js
@@ -13,4 +13,3 @@
 
 
 
-// Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune 
