d703d6304e54d945390294426f2c1b11b85a0d29
Thomas Szentirmay <tlszent@outlook.com>
2023-07-26 12:02:15 -0400

    set server up

--- db/seed.js
+++ db/seed.js
@@ -1,30 +1,47 @@
 const db = require('./connection');
-const {Planet, Like} = require('../models');
-const axios = require('axios');
+const { Planet, Like } = require('../models');
+// const axios = require('axios');
 
-const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+const planets = require('./planets');
+
 const planetData = [];
 
+db.once('open', async () => {
+  console.log('db connected!');
 
-db.once('open', () => {
-    console.log('db connected');
+  for (let planet in planets) {
+    const data = planets[planet];
 
-    planets.forEach(async (planet, index) => {
-        const url = `https://planets-17f2.onrender.com/planets/getPlanet?name=${planet}`;
-        const {data} = await axios.get(url);
-
-        planetData.push({
-            name: data.name,
-            picture: data.picture,
-            description: data.description
-        });
-
-        if(index === planets.length - 1) {
-            await Planet.insertMany(planetData);
-
-            console.log('Planets seeded successfully');
-
-            process.exit();
-        }
+    planetData.push({
+      name: data.name,
+      picture: data.picture,
+      description: data.description
     });
+  }
+
+  await Planet.insertMany(planetData);
+
+  console.log('Planets seeded successfully');
+
+  process.exit();
+
+
+  // planets.forEach(async (planet, index) => {
+  //   const url = `https://planets-17f2.onrender.com/planets/getPlanet?name=${planet}`;
+  //   const { data } = await axios.get(url);
+
+  //   planetData.push({
+  //     name: data.name,
+  //     picture: data.picture,
+  //     description: data.description
+  //   });
+
+  //   if (index === planets.length - 1) {
+  //     await Planet.insertMany(planetData);
+
+  //     console.log('Planets seeded successfully');
+
+  //     process.exit();
+  //   }
+  // });
 });
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+const db = require('./db/connection');
 
 
+// Middleware
+app.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Routes
+db.once('open', () => {
+    app.listen(PORT, () => console.log('Server started on port %s', PORT));
+})
