eb30e6545bc7c462111ccd882e9a1ae9553d42f5
Thomas Szentirmay <tlszent@outlook.com>
2023-07-26 14:03:36 -0400

    finished like route

--- routes/like_routes.js
+++ routes/like_routes.js
@@ -3,7 +3,19 @@ const {Planet, Like} = require('../models');
 
 // Add like to planet
 router.put('/like/:planet_id', async (req, res) => {
-    
+    const planet_id = req.params.planet_id;
+
+    const like = await like.create({
+        planet: planet_id
+    });
+
+    const planet = await Planet.findByIdAndUpdate(planet_id, {
+        $push: {
+            likes: like._id
+        }
+    }, {new: true});
+
+    res.json(planet);
 });
 
 module.exports = router;
