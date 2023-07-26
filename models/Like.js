7ebe64a9a7c4ca6fe2adcf85c195cbf99a6a1976
Thomas Szentirmay <tlszent@outlook.com>
2023-07-26 11:03:55 -0400

    built models

---
+++ models/Like.js
@@ -0,0 +1,14 @@
+const {Schema, model, Types} = require('mongoose');
+
+const likeSchema = new Schema({
+    planet: {
+        type: Types.ObjectId,
+        ref: 'Planet'
+    }
+}, {
+    timestamps: true
+});
+
+const Like = model('Like', likeSchema);
+
+module.exports = Like;
