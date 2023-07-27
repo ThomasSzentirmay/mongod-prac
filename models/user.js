ba28cc70a96ee7fa4e974a34a76b681cb5a31f41
Thomas Szentirmay <tlszent@outlook.com>
2023-07-27 17:39:34 -0400

    added method to user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const {Schema, model, Types} = require('mongoose');
+const { hash, compare } = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -17,6 +18,19 @@ const userSchema = new Schema({
             ref: 'Planet'
         }
     ]
+}, {
+    methods: {
+        validatePass: async function(formPassword) {
+            const is_valid = await compare(formPassword, this.password);
+        }
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.created) {
+        this.password = await hash(this.password, 10);
+        next();
+    }
 });
 
 const User = model('User', userSchema);
