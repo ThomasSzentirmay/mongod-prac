0749e99740b232c9a28d90fa756422d2ac5fb912
Thomas Szentirmay <tlszent@outlook.com>
2023-07-27 18:16:14 -0400

    built login route

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
-        minLength: 6
+        minLength: [6, 'Password must be at least 6 chars in length']
     },
     favorites: [
         {
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -10,6 +10,27 @@ router.post('/register', async (req, res) => {
     } catch(err) {
         console.log(err);
         res.status(401).send({
+            message: err.message.split(':')[2].trim()
+        });
+    }
+});
+
+// Login Route
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({
+            username: req.body.username
+        });
+
+        if (!user) throw new Error('No user found with that username');
+
+        const valid = await user.validatePass(req.body.password);
+
+        if(!valid) throw new Error('Your password is incorrect');
+
+        res.json(user);
+    } catch(err) {
+        res.status(401).send({
             message: err.message
         });
     }
