f6e024b5ecbfcc802f6ba42a9f55ac589da41caa
Thomas Szentirmay <tlszent@outlook.com>
2023-07-27 20:09:44 -0400

    added mongodb atlas cloud connection

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/planets_api_db');
+const isProduction = process.env.PORT;
+
+if(isProduction) {
+    mongoose.connect('mongodb+srv://tomlaszlo7:yfGeBUrZjmLOo4tF@cluster0.wdxmda8.mongodb.net/?retryWrites=true&w=majority');
+} else mongoose.connect('mongodb://127.0.0.1:27017/planets_api_db');
 
 module.exports = mongoose.connection;
